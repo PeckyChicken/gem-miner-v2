5c8c1b6e5f24865771d499823d96fd0fc980c1e0
Benjy <BenjyPeckChicken@gmail.com>
2025-03-04 14:03:40 +0000

    FEATURE: Music can now be played, with a `.json` file controlling the looping points. FEATURE: Music is now in a consistent form

--- Game/Scripts/node_2d.gd
+++ Game/Scripts/node_2d.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
 var PRESSED_KEYS = []
+@onready var Board: brd = $background/Board
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Fade.fade_out(0.5)
 	setup.call_deferred()
 
 func setup():
+	if Board.mode == "survival":
+		if Music.playing not in ["survival1","survival2"]:
+			Music.play(["survival1","survival2"].pick_random())
+	else:
+		if Board.mode != Music.playing:
+			Music.play(Board.mode)
 	Events.TileClicked.connect(tile_clicked)
-	$"background/Board".draw_background($background/background_tile)
-	$"background/Board".draw($background/tile)
+	Board.draw_background($background/background_tile)
+	Board.draw($background/tile)
 	$background/Pit.draw_background($background/tool_background_tile)
 	$background/Pit.fill()
 	$background/Pit.draw($background/tile)
@@ -22,28 +30,28 @@ func setup():
 	random_place([13])
 	random_place([13])
 
-	$"background/Board".draw($background/tile)
+	Board.draw($background/tile)
 
 func random_place(items:Array):
-	assert (0 in $"background/Board".board)
+	assert (0 in Board.board)
 	
 	var coordinates = []
-	for x in range($"background/Board".COLUMNS):
-		for y in range($"background/Board".ROWS):
+	for x in range(Board.COLUMNS):
+		for y in range(Board.ROWS):
 			coordinates.append(Vector2(x,y))
 	
 	coordinates.shuffle()
 	var selection
 	
 	for coordinate in coordinates:
-		if $"background/Board".get_square(coordinate) == 0:
+		if Board.get_square(coordinate) == 0:
 			selection = coordinate
 			break
 	
 	
-	$"background/Board".set_square(selection,items.pick_random())
-	$"background/Board".draw($background/tile,selection)
-	$"background/Board".pop_in(selection)
+	Board.set_square(selection,items.pick_random())
+	Board.draw($background/tile,selection)
+	Board.pop_in(selection)
 
 func _process(_delta: float) -> void:
 	pass
@@ -63,24 +71,24 @@ func _input(event):
 				select_pit_item(pos)
 
 func validate_tile_placement(location:Vector2):
-	if $"background/Board".get_square(location) != 0:
+	if Board.get_square(location) != 0:
 		return false
 	
-	var directions = $"background/Board".calculate_directions(location)
+	var directions = Board.calculate_directions(location)
 	
-	if  directions.all(func(c): return $"background/Board".get_square(c) == 0):
+	if  directions.all(func(c): return Board.get_square(c) == 0):
 		return false
 	
 	return true
 
 func add_bricks():
 	var count = 1
-	count += floori($"background/Board".level / 5.0)
-	if randi_range(0,4) < $"background/Board".level % 5:
+	count += floori(Board.level / 5.0)
+	if randi_range(0,4) < Board.level % 5:
 		count += 1
 	
 	for __ in range(count):
-		if 0 not in $"background/Board".board:
+		if 0 not in Board.board:
 			break
 		random_place([13])
 		Events.PlaySound.emit("Gameplay/brick_placed")
@@ -88,17 +96,17 @@ func add_bricks():
 func vanish_gems(location:Vector2):
 	var vanish = $background/vanish.duplicate()
 	vanish.show()
-	$"background/Board"._get_background_square(location).add_child(vanish)
-	vanish.position += Vector2($"background/Board".width/$"background/Board".COLUMNS/2,$"background/Board".height/$"background/Board".ROWS/2)
+	Board._get_background_square(location).add_child(vanish)
+	vanish.position += Vector2(Board.width/Board.COLUMNS/2,Board.height/Board.ROWS/2)
 	vanish.get_child(0).play("vanish")
 
 func create_game_tool(location:Vector2,clears,horizontal_matches,vertical_matches):
 	if len(clears) == 4: #Drills
 		if len(horizontal_matches) == 4: #Vertical drill
-			$"background/Board".set_square(location,11)
+			Board.set_square(location,11)
 		
 		elif len(vertical_matches) == 4: #Horizontal drill
-			$"background/Board".set_square(location,10)
+			Board.set_square(location,10)
 		else:
 			printerr("4 gems were cleared but there was not a line of 4 in either direction.\nTHIS SHOULD NOT BE HAPPENING.")
 			assert(false)
@@ -107,33 +115,33 @@ func create_game_tool(location:Vector2,clears,horizontal_matches,vertical_matche
 	
 	if len(horizontal_matches) >= 5 or len(vertical_matches) >= 5: #Diamonds
 		if len(horizontal_matches) >= 5 and len(vertical_matches) >= 5:
-			$"background/Board".set_square(location,9)
+			Board.set_square(location,9)
 			Events.PlaySound.emit("Diamond/double_create")
 		else:
 			#The diamonds are 4 offsets from the gems
-			$"background/Board".set_square(location,$"background/Board".get_square(location)+4)
+			Board.set_square(location,Board.get_square(location)+4)
 			Events.PlaySound.emit("Diamond/create")
 	
 	elif len(horizontal_matches) >= 3 and len(vertical_matches) >= 3: #Bombs
-		$"background/Board".set_square(location,12)
+		Board.set_square(location,12)
 		Events.PlaySound.emit("Bomb/create")
 	
 	for l in [location] + clears:
-		$"background/Board".draw($background/tile,l)
+		Board.draw($background/tile,l)
 	
-	await $"background/Board".pop_in(location)
+	await Board.pop_in(location)
 
 func create_lightning(point_a:Vector2,point_b:Vector2,color: Color) -> Node2D:
 	var lightning = $Lightning.duplicate()
 	var lightning_line: Line2D = lightning.get_node("Line")
-	lightning_line.position = Vector2($"background/Board".width/$"background/Board".ROWS / 2,$"background/Board".height/$"background/Board".COLUMNS / 2)
+	lightning_line.position = Vector2(Board.width/Board.ROWS / 2,Board.height/Board.COLUMNS / 2)
 	
-	var point_a_node = $"background/Board"._get_background_square(point_a)
+	var point_a_node = Board._get_background_square(point_a)
 	
 	var dx = point_b[0] - point_a[0]
 	var dy = point_b[1] - point_a[1]
-	dx *= $"background/Board".width/$"background/Board".ROWS
-	dy *= $"background/Board".height/$"background/Board".COLUMNS
+	dx *= Board.width/Board.ROWS
+	dy *= Board.height/Board.COLUMNS
 
 	lightning_line.points = [Vector2(0,0), Vector2(dx,dy)]
 	lightning.modulate = color
@@ -146,33 +154,33 @@ func create_lightning(point_a:Vector2,point_b:Vector2,color: Color) -> Node2D:
 func diamond(location:Vector2,type):
 	var lights = []
 	var squares = []
-	for square_x in $"background/Board".COLUMNS:
-		for square_y in $"background/Board".ROWS:
+	for square_x in Board.COLUMNS:
+		for square_y in Board.ROWS:
 			var square_location = Vector2(square_x,square_y)
-			if $"background/Board".get_square(square_location) == type-4:
-				$"background/Board"._get_foreground_square(square_location).align_for_animation()
-				$"background/Board"._get_foreground_square(square_location).animation_player.play("energized")
+			if Board.get_square(square_location) == type-4:
+				Board._get_foreground_square(square_location).align_for_animation()
+				Board._get_foreground_square(square_location).animation_player.play("energized")
 				squares.append(square_location)
 				
-				var lightning = create_lightning(location,square_location,$"background/Board".COLORS[type-5])
+				var lightning = create_lightning(location,square_location,Board.COLORS[type-5])
 				lightning.animation_player.play("energized")
 				lights.append(lightning)
 	
 	replace_squares(type-4,0)
 	Events.AddScore.emit(10)
-	$"background/Board".set_square(location,0)
+	Board.set_square(location,0)
 	Events.PlaySound.emit("Diamond/use")
-	$"background/Board"._get_foreground_square(location).animation_player.play("diamond")
+	Board._get_foreground_square(location).animation_player.play("diamond")
 	if len(lights) > 0:
 		await lights[0].animation_player.animation_finished
 	for light in lights:
 		light.queue_free()
 	$background/Brick.destroy_bricks(squares+[location])
 	
-	await $"background/Board"._get_foreground_square(location).animation_player.animation_finished
+	await Board._get_foreground_square(location).animation_player.animation_finished
 
 func evaluate_game_tool(location,calculate_combo=true):
-	var type = $"background/Board".get_square(location)
+	var type = Board.get_square(location)
 	var GAME_TOOLS = range(5,13)
 	
 	if type not in GAME_TOOLS:
@@ -206,9 +214,9 @@ func evaluate_game_tool(location,calculate_combo=true):
 	if type == 12:
 		clear_blast(location,1)
 		Events.PlaySound.emit("Bomb/use")
-		#await $"background/Board"._get_foreground_square(x,y).explode()
+		#await Board._get_foreground_square(x,y).explode()
 	
-	$"background/Board".draw($background/tile)
+	Board.draw($background/tile)
 	
 	if board_empty():
 		random_place($background/Pit.OPTIONS)
@@ -217,11 +225,11 @@ func evaluate_game_tool(location,calculate_combo=true):
 func calculate_tool_combo(location):
 	var combo = [location]
 	var GAME_TOOLS = range(5,13)
-	var directions = $"background/Board".calculate_directions(location)
+	var directions = Board.calculate_directions(location)
 	for direction in directions:
 		if direction == location:
 			continue
-		if $"background/Board".get_square(direction) in GAME_TOOLS:
+		if Board.get_square(direction) in GAME_TOOLS:
 			combo.append(direction)
 	
 	return combo
@@ -236,16 +244,16 @@ func _get_priority(item_type):
 	return priority
 
 func double_diamond(location:Vector2):
-	$"background/Board"._get_foreground_square(location).animation_player.play("double_diamond")
+	Board._get_foreground_square(location).animation_player.play("double_diamond")
 	Events.PlaySound.emit("Diamond/double_diamond")
 	var lights = []
-	for cur_x in $"background/Board".COLUMNS:
-		for cur_y in $"background/Board".ROWS:
+	for cur_x in Board.COLUMNS:
+		for cur_y in Board.ROWS:
 			var cur_location = Vector2(cur_x,cur_y)
-			var item = $"background/Board".get_square(cur_location)
-			var item_object = $"background/Board"._get_foreground_square(cur_location)
+			var item = Board.get_square(cur_location)
+			var item_object = Board._get_foreground_square(cur_location)
 			if item != 0:
-				var color = $"background/Board".COLORS[len(lights)%len($"background/Board".COLORS)]
+				var color = Board.COLORS[len(lights)%len(Board.COLORS)]
 				var light = create_lightning(location,cur_location,color)
 				lights.append(light)
 				light.animation_player.play("energized")
@@ -257,17 +265,17 @@ func double_diamond(location:Vector2):
 		light.queue_free()
 
 	Events.AddScore.emit(100)
-	$"background/Board".clear_board()
-	await $"background/Board"._get_foreground_square(location).animation_player.animation_finished
+	Board.clear_board()
+	await Board._get_foreground_square(location).animation_player.animation_finished
 
 func evaluate_tool_combo(location:Vector2,combo):
 	await $background/Line.animate_line_clear(location,combo,false)
-	var background_square = $"background/Board"._get_background_square(location)
+	var background_square = Board._get_background_square(location)
 	var _square
 	var _squares = []
 	for item in combo:
 		if item != location:
-			_square = $"background/Board"._get_foreground_square(item)
+			_square = Board._get_foreground_square(item)
 			_square.reparent(background_square)
 			_square.z_index -= 1
 			
@@ -280,7 +288,7 @@ func evaluate_tool_combo(location:Vector2,combo):
 	var top_two = [0,0]
 
 	for item in combo:
-		var item_type = $"background/Board".get_square(item)
+		var item_type = Board.get_square(item)
 		var priority = _get_priority(item_type)
 		var index = 0
 		for top in top_two:
@@ -288,7 +296,7 @@ func evaluate_tool_combo(location:Vector2,combo):
 				top_two[index] = item_type
 				break
 			index += 1
-		$"background/Board".set_square(item,0)
+		Board.set_square(item,0)
 	assert (0 not in top_two)
 	
 	if top_two[0] in [5,6,7,8] and top_two[1] in [5,6,7,8]:
@@ -333,9 +341,9 @@ func handle_lines(location:Vector2):
 		add_bricks()
 		return
 	
-	var type = $"background/Board".get_square(location)
-	$"background/Board".clear_gems(clears)
-	$"background/Board".set_square(location,type)
+	var type = Board.get_square(location)
+	Board.clear_gems(clears)
+	Board.set_square(location,type)
 	
 	Events.PlaySound.emit("Gameplay/break")
 	Events.AddScore.emit(10*(len(horizontal_matches+vertical_matches)))
@@ -347,57 +355,57 @@ func handle_lines(location:Vector2):
 	
 	if len(clears) == 3:
 		vanish_gems(location)
-		$"background/Board".set_square(location,0)
+		Board.set_square(location,0)
 	else:
 		await create_game_tool(location,clears,horizontal_matches,vertical_matches)
 	
 	for item in clears:
-		$"background/Board"._get_foreground_square(item).queue_free()
-		$"background/Board".draw($background/tile,item)
+		Board._get_foreground_square(item).queue_free()
+		Board.draw($background/tile,item)
 	
 	if board_empty():
 		random_place($background/Pit.OPTIONS)
 		Events.PlaySound.emit("Gameplay/place")
 
 func board_empty():
-	return $"background/Board".board.all(func(__): return __ == 0)
+	return Board.board.all(func(__): return __ == 0)
 
 func place(location,value):
-	$"background/Board".set_square(location,value)
+	Board.set_square(location,value)
 	Events.PlaySound.emit("Gameplay/place")
 	
-	$"background/Board".draw($background/tile,location)
-	$"background/Board".select(0,$background/tile,$background/selection_tile)
+	Board.draw($background/tile,location)
+	Board.select(0,$background/tile,$background/selection_tile)
 	$background/Pit.fill()
 	$background/Pit.draw($background/tile)
 	Events.AddScore.emit(1)
-	await $"background/Board".pop_in(location)
+	await Board.pop_in(location)
 
 func clear_line(location,direction:String):
 	assert (direction.to_lower() in ["h","v"])
 	var x_positions = []
 	var y_positions = []
 	if direction.to_lower() == "h":
-		x_positions = range($"background/Board".COLUMNS)
+		x_positions = range(Board.COLUMNS)
 	else:
-		x_positions.resize($"background/Board".COLUMNS)
+		x_positions.resize(Board.COLUMNS)
 		x_positions.fill(location.x)
 	
 	if direction.to_lower() == "v":
-		y_positions = range($"background/Board".ROWS)
+		y_positions = range(Board.ROWS)
 	else:
-		y_positions.resize($"background/Board".ROWS)
+		y_positions.resize(Board.ROWS)
 		y_positions.fill(location.y)
 	
 	
 	for cur_x in x_positions:
 		for cur_y in y_positions:
 			var cur_location = Vector2(cur_x,cur_y)
-			if $"background/Board".get_square(cur_location) != 0:
+			if Board.get_square(cur_location) != 0:
 				Events.AddScore.emit(10)
 			if cur_location != location:
 				evaluate_game_tool(cur_location,false)
-			$"background/Board".remove_square(cur_location)
+			Board.remove_square(cur_location)
 
 func clear_blast(location,radius:int):
 	var cur_x = location.x - radius
@@ -406,33 +414,33 @@ func clear_blast(location,radius:int):
 		cur_y = location.y - radius
 		while cur_y <= location.y + radius:
 			var cur_location = Vector2(cur_x,cur_y)
-			if $"background/Board".within_board(cur_location):
-				if $"background/Board".get_square(cur_location) != 0:
+			if Board.within_board(cur_location):
+				if Board.get_square(cur_location) != 0:
 					Events.AddScore.emit(10)
 				if cur_location != location:
 					evaluate_game_tool(cur_location,false)
-				$"background/Board".remove_square(cur_location)
+				Board.remove_square(cur_location)
 			cur_y += 1
 		cur_x += 1
 
 func select_pit_item(pos:int):
-	var temp = $"background/Board".selected
-	$"background/Board".select($background/Pit.get_item(pos),$background/tile,$background/selection_tile)
+	var temp = Board.selected
+	Board.select($background/Pit.get_item(pos),$background/tile,$background/selection_tile)
 	$background/Pit.set_item(pos,temp)
 	$background/Pit.draw($background/tile)
 
 func replace_squares(type,replacement):
 	var index = 0
 	var replaced_squares = []
-	for item in $"background/Board".board:
+	for item in Board.board:
 		if item == type:
 			var replace
 			if replacement is Array:
 				replace = replacement.pick_random()
 			else:
 				replace = replacement
-			$"background/Board".board[index] = replace
-			replaced_squares.append($"background/Board"._index_to_coords(index))
+			Board.board[index] = replace
+			replaced_squares.append(Board._index_to_coords(index))
 			
 			Events.AddScore.emit(10)
 		index += 1
@@ -441,19 +449,21 @@ func replace_squares(type,replacement):
 func tile_clicked(tile):
 	var location = Vector2(tile.x,tile.y)
 	if tile.type == Events.Type.board:
-		if $"background/Board".selected:
+		if Board.selected:
 			if validate_tile_placement(location):
-				if $"background/Board".mode == "obstacle":
-					$"background/Board".moves -= 1
+				if Board.mode == "obstacle":
+					Board.moves -= 1
 					Events.AddScore.emit(0)
-				await place(location,$"background/Board".selected)
+				await place(location,Board.selected)
 				handle_lines(location)
-				if $"background/Board".evaluate_game_over():
+				if Board.evaluate_game_over():
 					var game_over_ui: MarginContainer = $"Game over UI"
 					config.game_over = true
 					game_over_ui.position = Vector2(config.WINDOW_WIDTH/2,config.WINDOW_HEIGHT/2) - game_over_ui.size * 0.5
 					game_over_ui.set_anchors_preset(Control.PRESET_CENTER)
 					game_over_ui.show()
+					
+					Music.play("game_over")
 
 			else:
 				Events.PlaySound.emit("Gameplay/nomatch")
--- Music/MUSIC.gd
+++ Music/MUSIC.gd
@@ -1,1 +1,65 @@
 extends Node
+
+var playing = null
+
+var track_cache = {}
+var sound_file_cache = {}
+
+var players: Array[AudioStreamPlayer] = [AudioStreamPlayer.new(),AudioStreamPlayer.new()]
+
+var current_player = 0
+
+var path: String
+var loop_start: float
+var loop_end: float
+var sound: Resource
+
+var volume = 0.25
+
+func _ready() -> void:
+	for player in players:
+		player.volume_db = linear_to_db(volume)
+		add_child(player)
+
+func play(track:String):
+	stop()
+	var track_data
+	if track in track_cache:
+		track_data = track_cache[track]
+	else:
+		var config_file = load("res://Music/%s.json" % [track])
+		track_data = config_file.data
+		track_cache[track] = track_data
+	
+	path = track_data["path"]
+	loop_start = track_data["loop_start"]
+	loop_end = track_data["loop_end"]
+	
+	if path in sound_file_cache:
+		sound = sound_file_cache[path]
+	else:
+		sound = load(path)
+		sound_file_cache[path] = sound
+	
+	for player in players:
+		player.stream = sound
+	
+	players[0].play()
+	playing = track
+	
+	
+func stop():
+	for player in players:
+		player.stop()
+	playing = null
+	current_player = 0
+	
+func _process(_delta: float) -> void:
+	if playing:
+		players[current_player].volume_db = linear_to_db(volume)
+		if players[current_player].get_playback_position() >= loop_end:
+			current_player += 1
+			current_player %= len(players)
+			players[current_player].play(loop_start)
+			
+	
