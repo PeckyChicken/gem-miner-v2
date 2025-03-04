77701ca5cca66dbc94d3f02d16db8290c42278b3
Benjy <BenjyPeckChicken@gmail.com>
2025-03-04 12:03:11 +0000

    CLEANUP: All game items moved to their own folder UPDATE: Godot 4.4 added .uid files

---
+++ Game/Scripts/score.gd
@@ -0,0 +1,59 @@
+extends RichTextLabel
+
+@onready var board: brd = $"../../Board"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Events.AddScore.connect(add_score)
+	_update()
+
+func evaluate_next_level():
+	if board.score >= board.goal:
+		board.next_level()
+
+func add_score(score):
+	var score_to_add = score*board.level
+	
+	var final_score = score_to_add + board.score
+	const SCORE_TIME = 0.5
+	var STEP = max(3,board.level)
+	var sleep_time = SCORE_TIME/(float(score_to_add)/STEP)
+	
+	self_modulate = Color(300,300,300)
+	while board.score < final_score:
+		board.score += STEP
+		board.score = min(board.score,final_score)
+		_update()
+		await get_tree().create_timer(sleep_time).timeout
+	_update()
+	self_modulate = Color(1,1,1)
+	
+	evaluate_next_level()
+	
+func _update():
+	add_theme_font_size_override("normal_font_size", _calculate_font_size(board.score))
+	text = "[center]%s" % [board.score]
+	
+	$"../Level".add_theme_font_size_override("normal_font_size", _calculate_font_size(board.level))
+	$"../Level".text = "[center]%s" % [board.level]
+	
+	$"../Goal".add_theme_font_size_override("normal_font_size", _calculate_font_size(board.goal))
+	$"../Goal".text = "[center]%s" % [board.goal]
+	
+	$"../Moves".add_theme_font_size_override("normal_font_size", _calculate_font_size(board.moves))
+	$"../Moves".text = "[center]%s" % [board.moves]
+
+func _calculate_font_size(score) -> int:
+	var font_size = 50
+	var score_string = str(score)
+	
+	if len(score_string) < 3:
+		return font_size
+	
+	font_size -= 10* (len(score_string) - 2)
+	
+	return font_size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
