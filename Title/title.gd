f28596a71738f48795335762906f2cc85bca0ccb
Benjy <BenjyPeckChicken@gmail.com>
2025-04-05 06:45:13 +0100

    FEATURE: Options menu now displays in the middle of the screen

--- Title/title.gd
+++ Title/title.gd
@@ -3,6 +3,8 @@ extends Node2D
 const OPTIONS_MENU = preload("res://Global/options_menu.tscn")
 const HIGH_SCORE_MENU = preload("res://Global/high_scores.tscn")
 
+@onready var OFFSET = $"..".get_rect().size / 2.0
+
 func _ready() -> void:
 
 	await $"../Fade".fade_out(0.5)
@@ -17,13 +19,13 @@ func _on_pause_pressed() -> void:
 	get_tree().paused = true
 	var options_menu: OptionsMenu = OPTIONS_MENU.instantiate()
 	options_menu.pause_menu = null
-	options_menu.position = Vector2(-250,-250)
+	options_menu.position = -OFFSET
 	add_sibling(options_menu)
 
 func _on_high_score_pressed() -> void:
 	get_tree().paused = true
 	var high_score_menu: HighScores = HIGH_SCORE_MENU.instantiate()
-	high_score_menu.position = Vector2(-250,-250)
+	high_score_menu.position = -OFFSET
 	add_sibling(high_score_menu)
 
 func _on_close_pressed() -> void:
@@ -31,8 +33,8 @@ func _on_close_pressed() -> void:
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		var menu = get_node_or_null("options_menu")
-		var high_scores = get_node_or_null("high_score")
+		var menu = get_node_or_null("../options_menu")
+		var high_scores = get_node_or_null("../high_score")
 		if menu != null:
 			get_tree().paused = false
 			menu.queue_free()
