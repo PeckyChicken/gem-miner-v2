6a1806c17af5c128364e4dda12731f9bc98de77f
Benjy <BenjyPeckChicken@gmail.com>
2025-04-05 11:39:48 +0100

    CLEANUP: Items are now stored in the game manager as an Enum

--- Global/GAME_MANAGER.gd
+++ Global/GAME_MANAGER.gd
@@ -1,4 +1,6 @@
 extends Node
+class_name game_manager
+
 
 enum Mode {
 	survival,
@@ -12,6 +14,37 @@ enum Preview {
 	basic,
 	advanced
 }
+
+enum Item {
+	AIR,
+	RUBY,
+	TOPAZ,
+	EMERALD,
+	SAPPHIRE,
+	RED_DIAMOND,
+	YELLOW_DIAMOND,
+	GREEN_DIAMOND,
+	BLUE_DIAMOND,
+	RAINBOW_DIAMOND,
+	H_DRILL,
+	V_DRILL,
+	BOMB,
+	BRICK,
+	RED_BRICK,
+	YELLOW_BRICK,
+	GREEN_BRICK,
+	BLUE_BRICK,
+	CROSS
+}
+
+const GEMS = [Item.RUBY,Item.TOPAZ,Item.EMERALD,Item.SAPPHIRE]
+
+const DIAMONDS = [Item.RED_DIAMOND,Item.YELLOW_DIAMOND,Item.GREEN_DIAMOND,Item.BLUE_DIAMOND]
+
+var GAME_TOOLS = [Item.H_DRILL,Item.V_DRILL,Item.BOMB] + DIAMONDS
+
+const BRICKS = [Item.BRICK,Item.RED_BRICK,Item.YELLOW_BRICK,Item.GREEN_BRICK,Item.BLUE_BRICK]
+const COLORED_BRICKS = [Item.RED_BRICK,Item.YELLOW_BRICK,Item.GREEN_BRICK,Item.BLUE_BRICK]
 
 var preview: Preview
 
