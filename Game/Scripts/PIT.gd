688699c524b9834cbd0683d9f0468f46b20cb89b
Benjy <BenjyPeckChicken@gmail.com>
2025-05-10 14:19:41 +0100

    FEATURE: Pit now aligns with the center ground instead of the board.

--- Game/Scripts/PIT.gd
+++ Game/Scripts/PIT.gd
@@ -1,34 +1,50 @@
-extends Node
+extends Control
 
-const SQUARES = 3
+const SQUARE_COUNT = 3
 const OPTIONS = Game.GEMS
 var TOOLS = range(5,13)
 var pit: Array[int] = []
+@onready var Board: brd = $"../Board"
+
 var background_tiles = []
 var foreground_tiles: Array[GameTile] = []
-var board_padding = 0.5 #Number of squares the pit is from the board.
+var screen_padding = 0.25 #Number of squares the pit is from the bottom of the screen.
 
 func draw_background():
 	var background_tile = $"../pit_background_tile"
 	for tile in background_tiles:
 		tile.queue_free()
 	background_tiles.clear()
-	var square_width = $"../Board".width / $"../Board".COLUMNS
-	var square_height = $"../Board".height / $"../Board".ROWS
 	
-	var cur_x = $"../Board".start_x
-	var y = $"../Board".start_y + $"../Board".height + (board_padding*square_height)
-	var x_margin = ($"../Board".width-square_width) / float(SQUARES)
+	var square_size = Vector2(Board.width/Board.COLUMNS,Board.height/Board.ROWS)
+	var start_pos = -Vector2(Board.width / 2.0,square_size.y*(screen_padding+1))
 	
-	for x in range(SQUARES):
+	
+	if SQUARE_COUNT == 1:
 		var new_tile = background_tile.duplicate()
-		new_tile.x = x
-		new_tile.position = Vector2(cur_x,y)
+		new_tile.position = start_pos*Vector2(0,1)
+
 		new_tile.type = Events.Type.pit
 		new_tile.show()
 		add_child(new_tile)
 		background_tiles.append(new_tile)
-		cur_x += x_margin + square_width
+		return
+	elif SQUARE_COUNT == 0:
+		return
+	elif SQUARE_COUNT < 0:
+		printerr("Must have a positive or zero number of pit squares.")
+		assert (false)
+	
+	
+	for x in range(SQUARE_COUNT):
+		var new_tile = background_tile.duplicate()
+		var rel_pos = Vector2((x/float(SQUARE_COUNT-1))*(Board.width-square_size.x),0)
+		new_tile.x = x
+		new_tile.position = start_pos + rel_pos
+		new_tile.type = Events.Type.pit
+		new_tile.show()
+		add_child(new_tile)
+		background_tiles.append(new_tile)
 
 func draw():
 	var tile_sprite = $"../tile"
@@ -63,7 +79,7 @@ func get_item(index):
 
 func empty():
 	pit.clear()
-	for __ in range(SQUARES):
+	for __ in range(SQUARE_COUNT):
 		pit.append(0)
 
 func fill(color = null):
